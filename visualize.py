5d19c4a015fd3332eb7aa3096f6a34f28c99ffd0
Johnny Edwards <kineticblack@hotmail.com>
2016-03-03 22:53:52 +1300

    Updated the visualization.

--- visualize.py
+++ visualize.py
@@ -2,14 +2,22 @@ import sqlite3
 import seaborn
 import datetime
 import numpy
+import matplotlib
 import matplotlib.pyplot as plt
 
-conn = sqlite3.connect('example_net_data.db')
+matplotlib.rc('xtick', labelsize=12)
+matplotlib.rc('ytick', labelsize=12)
+
+conn = sqlite3.connect('net_data.db')
 data = conn.execute("SELECT * FROM entries;").fetchall()
 
 entry_datetime = [datetime.datetime.strptime(d[1],'%Y-%m-%d %H:%M:%S.%f') for d in data]
 download = [entry[3] for entry in data]
 
+plt.figure(figsize=(19.20, 12.80))
+plt.title('Download Speed', fontsize=22, fontweight='bold')
+plt.xlabel('DateTime', fontsize=18)
+plt.ylabel('Speed (Mbp/s)', fontsize=18)
 plt.plot(entry_datetime, download)
 
-plt.show()
+plt.savefig('.\\graphs\\test.jpg', format='jpg')
